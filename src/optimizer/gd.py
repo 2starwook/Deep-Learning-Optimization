8ed5af50db707e919c9ab1753663f1d44d4d3f82
Isaac Lee <2starwook@gmail.com>
2024-04-15 08:15:31 -0500

    Refactor

--- src/optimizer/gd.py
+++ src/optimizer/gd.py
@@ -2,16 +2,15 @@ import tensorflow as tf
 
 
 class GradientDescent(tf.Module):
-    def __init__(self, learning_rate=0.07):
+    def __init__(self, lr=0.07):
         """
-        Smaller learning rates causes it to get stuck at inflection point
-        Larger learning rates has a risk of having exploding gradients in early interations
+        lr (float, optional): learning rate (default 0.07)
+            Smaller learning rates causes it to get stuck at inflection point
+            Larger learning rates has a risk of having exploding gradients in early interations
         """
-        # Initialize parameters
-        self.learning_rate = learning_rate
-        self.title = f"Gradient descent optimizer: learning rate={self.learning_rate}"
+        self.lr = lr
+        self.title = f"Gradient descent optimizer: learning rate={self.lr}"
 
     def apply_gradients(self, grads, vars):
-        # Update variables
-        for i, (grad, var) in enumerate(zip(grads, vars)):
-            var.assign_sub(self.learning_rate*grad)
+        for grad, var in zip(grads, vars):
+            var.assign(var - self.lr*grad)
