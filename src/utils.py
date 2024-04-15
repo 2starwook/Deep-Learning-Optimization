e235498ec217a6449dbbaf54dfc1a31d4121c003
Isaac Lee <2starwook@gmail.com>
2024-04-15 13:43:06 -0500

    Move partial sample codes to sample directory

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,9 @@
-import os
-from os.path import sep
-from collections import Counter
-
 import numpy as np
 import tensorflow as tf
-import tarfile
-import fnmatch
 from tensorflow.keras.datasets import mnist
 from tensorflow.python.keras import Model
 from tensorflow import Module
 
-def binarise(y_pred):
-    threshold, upper, lower = 0.5, 1, 0
-    y_pred[y_pred >= threshold] = 1.
-    y_pred[y_pred < threshold] = 0.
-    return y_pred
-
-
-def extract_file(cls, dir: str, ext: str = "*.tar"):
-    tar_files = [f for f in os.listdir(dir) if fnmatch.fnmatch(f, ext)]
-    for file in tar_files:
-        task_path = f"{dir}{sep}{file}"
-        with tarfile.open(task_path, 'r') as tar:
-            tar.extractall(f"{dir}{sep}")
-
-def get_max_occurence_value(x: list):
-    inv_map = {v: k for k, v in Counter(x).items()}
-    return inv_map[max(inv_map.keys())]
 
 def smooth(data: list[float], weight: float = 0.9) -> list[float]:
     """
