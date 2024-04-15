6276e2eaa25ff0a6bfe0ab3a8e92a8a1fc84311c
Isaac Lee <2starwook@gmail.com>
2024-04-15 08:15:49 -0500

    Refactor

--- src/optimizer/gdm.py
+++ src/optimizer/gdm.py
@@ -1,23 +1,21 @@
 import tensorflow as tf
+import numpy as np
 
 
 class GradientDescentMomentum(tf.Module):
-    def __init__(self, learning_rate=1e-3, momentum=0.7):
-        # Initialize parameters
-        self.learning_rate = learning_rate
+    def __init__(self, lr=0.07, momentum=0.9):
+        """
+        lr (float, optional): learning rate (default 0.001)
+        momentum (float, optional): momentum factor (default: 0.7)
+        """
+        self.lr = lr
         self.momentum = momentum
-        self.changes = list()
         self.title = f"Gradient descent momentum optimizer: "\
-            f"learning rate={self.learning_rate} / "\
+            f"learning rate={self.lr} / "\
             f"momentum ={self.momentum}"
 
     def apply_gradients(self, grads, vars):
-        if len(self.changes) <= 1: # self.changes gets wrapped by ListWrapper
-            self.changes = [0. for _ in range(len(vars))]
-        
-        # Update variables 
-        for i, (grad, var) in enumerate(zip(grads, vars)):
-            change = self.changes[i]
-            curr_change = self.learning_rate*grad + self.momentum*change
-            var.assign_sub(curr_change)
-            self.changes[i] = curr_change
+        for grad, var in zip(grads, vars):
+            v = tf.Variable(np.zeros(var.get_shape(), dtype='float32'), name='v')
+            v.assign(self.momentum*v - self.lr*grad)
+            var.assign(var + (self.momentum**2)*v - (1 + self.momentum)*self.lr*grad)
