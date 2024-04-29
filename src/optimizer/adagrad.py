500bf5573a47162b176197cd6f8d085175e6cccc
Isaac Lee <2starwook@gmail.com>
2024-04-29 09:24:36 -0500

    reset Adagrad parameter default values

--- src/optimizer/adagrad.py
+++ src/optimizer/adagrad.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 class AdaGrad(tf.Module):
-    def __init__(self, lr=0.07, eps=np.float32(1e-2)):
+    def __init__(self, lr=0.001, eps=1e-8):
         """
-        lr (float, optional): learning rate (default 0.07)
-        eps (float, optional): small positive value to avoid divide by zero error
+        lr (float, optional): learning rate (default 0.001)
+        eps (float, optional): small positive value to avoid divide by zero error 
+            (default 1e-8)
         """
         self.lr = lr
         self.eps = eps
